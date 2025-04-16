95f71714483f28208f06384c95bbc41e0445bc9f
lin <lin1016344779@gmail.com>
2025-04-16 14:25:05 +0800

    proj1b: Implemented the Constructor

---
+++ proj1b/src/deque/ArrayDeque61B.java
@@ -0,0 +1,66 @@
+package deque;
+
+import java.util.List;
+
+public class ArrayDeque61B<T> implements Deque61B<T>{
+    private T[] items;
+    private int size;
+    private int nextFirst;
+    private int nextLast;
+
+    /**
+     * Creates an empty array deque with initial capacity of 8.
+     */
+    @SuppressWarnings("unchecked")
+    public ArrayDeque61B() {
+        items = (T[]) new Object[8];
+        size = 0;
+        nextFirst = 0;
+        nextLast = 1;
+    }
+    
+    @Override
+    public void addFirst(T x) {
+
+    }
+
+    @Override
+    public void addLast(T x) {
+
+    }
+
+    @Override
+    public List<T> toList() {
+        return List.of();
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public T removeFirst() {
+        return null;
+    }
+
+    @Override
+    public T removeLast() {
+        return null;
+    }
+
+    @Override
+    public T get(int index) {
+        return null;
+    }
+
+    @Override
+    public T getRecursive(int index) {
+        return null;
+    }
+}
